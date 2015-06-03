351569009d1667d02b7565a1575f2a1434b52150
João Paulo Cercal <jpcercal@gmail.com>
2015-06-03 00:59:34 -0300

    Added the AngularJS to render a class name of widgets.

--- assets/js/controllers.js
+++ assets/js/controllers.js
@@ -4,11 +4,36 @@
 
 var projectsControllers = angular.module('projectsControllers', []);
 
-projectsControllers.controller('ProjectsCtrl', ['$scope', '$http',
-    function($scope, $http) {
-        $http.get('projects.json').success(function(data) {
-            $scope.projects = data;
-        });
+projectsControllers.controller('ProjectsCtrl', ['$scope', '$http', function($scope, $http) {
 
-        $scope.orderProp = 'date';
-    }]);
+    $scope.orderProp = 'date';
+
+    $http.get('projects.json').success(function(data) {
+        $scope.projects = data;
+    });
+
+    $scope.getTimelineColorClass = function (currentIndex) {
+
+        var numberReduce = function(number) {
+
+            var numberMaxItems = 6;
+
+            return number >= numberMaxItems ? numberReduce(number - numberMaxItems) : number;
+        };
+
+        switch(numberReduce(currentIndex)) {
+            case 0:
+                return 'bgcolor-red';
+            case 1:
+                return 'bgcolor-orange';
+            case 2:
+                return 'bgcolor-yellow';
+            case 3:
+                return 'bgcolor-green';
+            case 4:
+                return 'bgcolor-blue';
+            case 5:
+                return 'bgcolor-darkblue';
+        }
+    };
+}]);
